d0e952f3a94275c543c0ef708783a5d5d8a005db
vladimirStary <puffychords@gmail.com>
2024-03-06 16:22:45 +0300

    socket-changes

--- src/auth/filters/ws.exception.filter.ts
+++ src/auth/filters/ws.exception.filter.ts
@@ -6,6 +6,7 @@ import { Socket } from 'socket.io'
 export class SocketExceptionsFilter extends BaseWsExceptionFilter {
   catch(exception: any, host: ArgumentsHost) {
     const client = host.switchToWs().getClient() as Socket;
+    console.log("SOCKET EXPIRED")
     client.emit('error', {
       exception: exception.name,
       status: exception.status
--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -15,46 +15,47 @@ import { SocketExceptionsFilter } from "src/auth/filters/ws.exception.filter";
 import { SocketUserGuard } from "src/auth/guards/socket.user.guard";
 import { ChatService } from "./chat.service";
 
-export interface CommentPayload {
-  userId: number;
-  collectionItemId: number;
-  text: string;
-}
 @WebSocketGateway({
   cors: true,
   namespace: '/chat'
 })
 @UseFilters(SocketExceptionsFilter)
 export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
-    constructor(
-      private chatService: ChatService
-    ) {}
-    @WebSocketServer() server: Server;
-    private connectedClients: Socket[] = [];
-    handleConnection(client: Socket) { this.connectedClients.push(client) }
-    handleDisconnect(client: Socket) { this.connectedClients.splice(this.connectedClients.indexOf(client), 1) }
+  constructor(
+    private chatService: ChatService
+  ) {}
+  @WebSocketServer() server: Server;
+  private connectedClients: Socket[] = [];
+  handleConnection(client: Socket) { 
+    console.log("connected");
+    this.connectedClients.push(client)
 
-    @UseGuards(SocketUserGuard)
-    @SubscribeMessage('messageToServer')
-    async handleSendMessage(client: Socket, message: { room: string; text: string;}) {
-      await this.chatService.sendMessage(Number(client.handshake.headers.userId), message.room, message.text);
-      await this.server.to(message.room).emit('messageToServer', {
-        message: message.text, 
-        client: client.id, 
-        userId: Number(client.handshake.headers.userId),
-        username: client.handshake.headers.userName.toString()
-      })
-    }
-    @UseGuards(SocketUserGuard)
-    @SubscribeMessage('leaveChat')
-    handleLeaveChar(client: Socket, room: string) {
-        client.leave(room);
-        client.emit('leaveRoom', room)
-    }
-    @UseGuards(SocketUserGuard)
-    @SubscribeMessage('joinChat')
-    handleJoinChat(client: Socket, room: string) {
-        client.join(room);
-        client.emit('joinedChat', room)
-    }
+  }
+  handleDisconnect(client: Socket) { this.connectedClients.splice(this.connectedClients.indexOf(client), 1) }
+  
+
+  @UseGuards(SocketUserGuard)
+  @SubscribeMessage('messageToServer')
+  async handleSendMessage(client: Socket, message: { room: string; text: string;}) {
+    await this.chatService.sendMessage(Number(client.handshake.headers.userId), message.room, message.text);
+    await this.server.to(message.room).emit('messageToServer', {
+      message: message.text, 
+      client: client.id, 
+      userId: Number(client.handshake.headers.userId),
+      username: client.handshake.headers.userName.toString()
+    })
+  }
+  @UseGuards(SocketUserGuard)
+  @SubscribeMessage('leaveChat')
+  handleLeaveChar(client: Socket, room: string) {
+    client.leave(room);
+    client.emit('leaveRoom', room)
+  }
+  @UseGuards(SocketUserGuard)
+  @SubscribeMessage('joinChat')
+  handleJoinChat(client: Socket, room: string) {
+    console.log("joined")
+    client.join(room);
+    client.emit('joinedChat', room)
+  }
 }
